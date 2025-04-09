d53a01371a25a43611d1a456d81841d2a53b75f2
Isaac <isaacjnelson29@gmail.com>
2025-04-09 11:22:57 -0500

    finished kogge stone alg

--- prefix_scan.cu
+++ prefix_scan.cu
@@ -5,18 +5,34 @@
 
 #define THREADS_PER_BLOCK 256
 
-__global__ void kogge_stone_inclusive(unsigned int* output, unsigned int* input){
+__global__ void kogge_stone_inclusive(unsigned int* output, unsigned int* input, 
+    unsigned int* partialSums){
     // boilerplate
     unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
     output[i] = input[i];
     __synchthreads();
 
     for (unsigned int stride = 1; stride <= BLOCK_DIM/2; stride*=2){
+        float v;
+        if(threadIdx.x >= stride){
+            v = output[i - stride];
+        }
+        __synchthreads();
+        if(threadIdx.x >= stride){
+            output[i] += v;
+        }
+        __synchthreads();
+    }
 
+    if(threadIdx.x == BLOCK_DIM - 1){
+        partialSums[blockIdx.x] = output[i];
     }
 }
 
+
 int main(){
+
+
 
     return 0;
 }
