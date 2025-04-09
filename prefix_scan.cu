495a7018fb1635678c536d12fb44f04b499d6437
Isaac <isaacjnelson29@gmail.com>
2025-04-09 11:12:13 -0500

    kogge_stone algorithm implemented

--- prefix_scan.cu
+++ prefix_scan.cu
@@ -1,8 +1,19 @@
 #include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <cuda_runtime.h>
 
-__global__ void kogge_stone_inclusive(){
+#define THREADS_PER_BLOCK 256
 
+__global__ void kogge_stone_inclusive(unsigned int* output, unsigned int* input){
+    // boilerplate
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    output[i] = input[i];
+    __synchthreads();
 
+    for (unsigned int stride = 1; stride <= BLOCK_DIM/2; stride*=2){
+
+    }
 }
 
 int main(){
